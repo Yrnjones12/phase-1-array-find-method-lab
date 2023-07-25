1625339d43eef74a9ac18ab2f6bac72d06c8881a
Yrnjones12 <jonesmunene126@gmail.com>
2023-07-25 04:09:52 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function superbowlWin(array) {
+  const winningYear = array.find(function (item) {
+    return item.result === "W";
+  });
+
+  if (winningYear) {
+    return winningYear.year;
+  } else {
+    return undefined;
+  }
+}
